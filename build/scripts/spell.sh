d2aae5aee2c59cdfe39f3dd7bd9eaeeebb75b5a7
init current directory[i] <urbanjost@comcast.net>
2020-07-02 21:43:26 -0400

    Add list and spellcheck to Makefile

---
+++ build/scripts/spell.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+###############################################################################################################
+#@(#)aspell -l mimicks the standard unix spell(1) program, roughly.  (John S. Urban)
+###############################################################################################################
+export LC_ALL='C'
+if inpath aspell
+then
+   #cat "$@" | aspell -l --mode=none | sort -u # old aspell
+   cat "$@" | aspell --mode=none list | sort -u |xargs -n 5|column -t
+elif inpath ispell
+then
+   cat "$@" | ispell -l | sort -u|xargs -n 5|column -t
+elif inpath spell
+then
+   cat "$@" | spell|sort -u|xarg -n 5|column -t
+else
+  echo 'Could not find spell, aspell, ispell' 1>&2
+fi
+###############################################################################################################
+exit
+
+
