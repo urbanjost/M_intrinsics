e21de26c796a2229386e5918d26024459c84a564
init current directory[i] <urbanjost@comcast.net>
2020-12-06 03:26:08 -0500

    backspace demo was not being generated

--- src/M_intrinsics.f90
+++ src/M_intrinsics.f90
@@ -1720,7 +1720,7 @@ textblock=[character(len=256) :: &
 'SYNOPSIS', &
 '   BACKSPACE file-unit-number', &
 '   BACKSPACE ([UNIT=]file-unit-number][,IOMSG=iomsg-variable]', &
-'   [,IOSTAT=scalar-int-variable][,ERR=label])', &
+'             [,IOSTAT=scalar-int-variable][,ERR=label])', &
 '', &
 'DESCRIPTION', &
 '   Execution of a BACKSPACE statement causes the file connected to the', &
@@ -1774,14 +1774,14 @@ textblock=[character(len=256) :: &
 '   character(len=256) :: mssge', &
 '   integer            :: i', &
 '   integer            :: ios', &
-'      open(10,file=''demo_backspace.txt'') ! open a file', &
+'      open(10,file=''dem_backspace.txt'') ! open a file', &
 '      do i=1,100                         ! write lines to it', &
 '         write(10,''(a,i0)'') ''line '',i', &
 '      enddo', &
 '      do i=1,10                          ! back up several lines', &
 '         backspace(10, iostat=ios,iomsg=mssge)', &
 '         if(ios.ne.0)then', &
-'                 write(*,''(*(a))'') ''*demo_backspace* ERROR:'',mssge', &
+'                 write(*,''(*(a))'') ''*dem_backspace* ERROR:'',mssge', &
 '         endif', &
 '      enddo', &
 '      read(10,''(a)'')line', &
@@ -7900,17 +7900,16 @@ textblock=[character(len=256) :: &
 '   result = minloc(array [, mask])', &
 '', &
 'DESCRIPTION', &
-'   Determines the location of the element in the array with the minimum', &
-'   value, or, if the DIM argument is supplied, determines the', &
+'   Determines the location of the element in the array with the', &
+'   minimum value, or, if the DIM argument is supplied, determines the', &
 '   locations of the minimum element along each row of the array in the', &
-'   DIM direction. If MASK is present, only the elements for', &
-'   which MASK is .true. are considered. If more than one', &
-'   element in the array has the minimum value, the location returned is', &
-'   that of the first such element in array element order. If the array has', &
-'   zero size, or all of the elements of MASK are .false., then', &
-'   the result is an array of zeroes. Similarly, if DIM is supplied', &
-'   and all of the elements of MASK along a given row are zero, the', &
-'   result value for that row is zero.', &
+'   DIM direction. If MASK is present, only the elements for which MASK', &
+'   is .true. are considered. If more than one element in the array has', &
+'   the minimum value, the location returned is that of the first such', &
+'   element in array element order. If the array has zero size, or all', &
+'   of the elements of MASK are .false., then the result is an array of', &
+'   zeroes. Similarly, if DIM is supplied and all of the elements of MASK', &
+'   along a given row are zero, the result value for that row is zero.', &
 '', &
 'ARGUMENTS', &
 '   ARRAY    Shall be an array of type INTEGER, REAL, or CHARACTER.', &
@@ -7921,13 +7920,12 @@ textblock=[character(len=256) :: &
 '            and conformable with ARRAY.', &
 '', &
 'RETURN VALUE', &
-'   If DIM is absent, the result is a rank-one array with a length', &
-'   equal to the rank of ARRAY. If DIM is present, the result', &
-'   is an array with a rank one less than the rank of ARRAY, and a', &
-'   size corresponding to the size of ARRAY with the DIM', &
-'   dimension removed. If DIM is present and ARRAY has a rank', &
-'   of one, the result is a scalar. In all cases, the result is of default', &
-'   INTEGER type.', &
+'   If DIM is absent, the result is a rank-one array with a length equal', &
+'   to the rank of ARRAY. If DIM is present, the result is an array with', &
+'   a rank one less than the rank of ARRAY, and a size corresponding to', &
+'   the size of ARRAY with the DIM dimension removed. If DIM is present', &
+'   and ARRAY has a rank of one, the result is a scalar. In all cases,', &
+'   the result is of default INTEGER type.', &
 '', &
 'EXAMPLE', &
 '  sample program:', &
@@ -7935,14 +7933,26 @@ textblock=[character(len=256) :: &
 '    program demo_minloc', &
 '    implicit none', &
 '    integer,save :: ints(3,5)= reshape([&', &
-'       1,  2,  3,  4,  5, &', &
-'      10, 20, 30, 40, 50, &', &
-'      11, 22, 33, 44, 55  &', &
+'       4, 10,  1,  7, 13, &', &
+'       9, 15,  6, 12,  3, &', &
+'      14,  5, 11,  2,  8  &', &
 '    ],shape(ints),order=[2,1])', &
 '    write(*,*) minloc(ints)', &
 '    write(*,*) minloc(ints,dim=1)', &
 '    write(*,*) minloc(ints,dim=2)', &
+'    ! where in each column is the smallest number .gt. 10 ?', &
+'    write(*,*) minloc(ints,dim=2,mask=ints.gt.10)', &
+'    ! a one-dimensional array with dim=1 explicitly listed returns a scalar', &
+'    write(*,*) minloc(pack(ints,.true.),dim=1) ! scalar', &
 '    end program demo_minloc', &
+'', &
+'  Results:', &
+'', &
+'      1  3        ', &
+'      1  3  1  3  2', &
+'      3  5  4     ', &
+'      5  4  3     ', &
+'      7           ', &
 '', &
 'STANDARD', &
 '   [[Fortran 95]] and later', &
